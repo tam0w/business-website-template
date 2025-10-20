7822e48c6d6c99e550fe6539680dd1323f70c9e3
Tamim <work.tamimmuhammed@gmail.com>
2025-10-20 15:11:23 +0530

    feat: init commit

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import './styles.css'
+import { ReactNode } from 'react'
+import './globals.css'
 
-export const metadata = {
-  description: 'A blank template using Payload in a Next.js app.',
-  title: 'Payload Blank Template',
+type LayoutProps = {
+  children: ReactNode
 }
 
-export default async function RootLayout(props: { children: React.ReactNode }) {
-  const { children } = props
-
+const Layout = ({ children }: LayoutProps) => {
   return (
-    <html lang="en">
-      <body>
-        <main>{children}</main>
+    <html>
+      <body className='h-dvh bg-black'>
+        {children}
       </body>
     </html>
   )
 }
+
+export default Layout
